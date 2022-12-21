38f9e4b2b5b57e36eb9f4140c91f92c8add30759
hakankosum <hakan.kosum@gmail.com>
2022-12-21 14:01:14 +0300

    login controller update

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -3,20 +3,18 @@ import 'package:get/get.dart';
 
 import '../../roots/app_pages.dart';
 
-class LoginController extends GetxController{
+class LoginController extends GetxController {
   TextEditingController usernameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-  bool checkEmtpyFields(){
-    if (usernameController.text.isNotEmpty&&passwordController.text.isNotEmpty){
+  bool checkEmtpyFields() {
+    if (usernameController.text.isNotEmpty &&
+        passwordController.text.isNotEmpty) {
       return true;
-      
-    }
-    else{
-      Get.snackbar("error", "empty fields",snackPosition: SnackPosition.BOTTOM);
+    } else {
+      Get.snackbar("ERROR", "Empty fields",
+      colorText: Colors.white,
+          snackPosition: SnackPosition.BOTTOM);
       return false;
     }
-
-
   }
-  
 }
